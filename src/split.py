c394b7d6d0dfb32e13697baa77b1bea1455b4ea9
conruiyS2023 <congruiy@andrew.cmu.edu>
2023-11-29 17:01:14 -0800

    Add split script for training set

---
+++ src/split.py
@@ -0,0 +1,19 @@
+import os
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+def split(path: str = 'train.csv'):
+    training_data = pd.read_csv(os.path.join('./data/', path))
+    train_set, val_set = train_test_split(training_data, train_size=0.8, random_state=42)
+    pd.DataFrame.to_csv(train_set, os.path.join('./data/train_val.csv'))
+    pd.DataFrame.to_csv(val_set, os.path.join('./data/val_val.csv'))
+
+
+def main():
+    split()
+
+
+if __name__ == '__main__':
+    main()
